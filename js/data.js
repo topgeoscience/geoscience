ff45cd3a3071272158a2f1841b9ed5c15421b652
gaohr <gaohrgao@163.com>
2019-10-29 23:04:51 +0800

    update

--- js/data.js
+++ js/data.js
@@ -56,23 +56,64 @@ function select(n) {
 $("#query-01").click(function() {
 	alert("关键字查询");
 });
-		
+	
+// 全部条件
+var data_type = ["地理", "水文", "地质", "环境", "GIS", "遥感"];
+var data_form = ["矢量", "栅格", "其他"];
+var data_online = ["在线", "离线"];
+	
+// 条件查询
 $("#query-02").click(function() {
+	// 先清空列表
+	$("#data-list").html("");
+	$('#data-list').append("<div class='col-md-4'></div><div class='col-md-4'><i id='data-loading' class='loading-3'></div><div class='col-md-4'></div>")
+	
 	var selectList = new Array();
 	var slen = $(".select i").length;
 	for(var i = 0; i < slen; i++) {
 		selectList.push($(".select i").eq(i).html());
 	}
-	alert(selectList.join(";"))
+	// 拼接条件数组
+	var data_type_new = data_type;
+	var data_form_new = data_form;
+	var data_online_new = data_online;
+	for(var d = 0; d < selectList.length; d++) {
+		if(data_type.indexOf(selectList[d]) != -1) {
+			data_type_new = [selectList[d]];
+		} 
+		if(data_form.indexOf(selectList[d]) != -1) {
+			data_form_new = [selectList[d]];
+		} 
+		if(data_online.indexOf(selectList[d]) != -1) {
+			data_online_new = [selectList[d]];
+		} 
+	}
+	// 更新列表
+	setTimeout(function() {
+		$("#data-loading").remove();
+		var data_count = 0;
+		for(var k = 0; k < data_list.length; k++) {
+			if(data_type_new.indexOf(data_list[k].type) != -1 && data_form_new.indexOf(data_list[k].format) != -1 && data_online_new.indexOf(data_list[k].online) != -1) {
+				$("#data-list").append(showDataList(data_list, k));
+				data_count += 1;
+			}
+		}
+		if(data_count == 0) {
+			$("#data-list").html("未查询到结果！");
+		}
+				
+	}, 1500);
 });
 	
 	
 	
 for(var i = 0; i < data_list.length; i++) {
-	$("#data-list").append("<div class='col-md-4' id='data-x-" + data_list[i].id + "' name='" + data_list[i].id + "'><div class='card'><p class='card_class'>" + data_list[i].type + "</p><img class='card_picture' src='../img/data/data-" + data_list[i].id + ".jpg' alt=''><p class='card_title'>" + data_list[i].name + "</p><div class='card-infos'><p class='card_text'>" + data_list[i].intro + "</p><p class='card_type'>数据格式：<span>" + data_list[i].format + "</span></p><p class='card_size'>数据大小：<span>" + data_list[i].size + "</span></p><p class='card_online'>获取方式：<span id='card_online_span_" + data_list[i].id + "'>" + data_list[i].online + "</span><hr><p class='card_owner'>数据提供者：<img class='seller_avatar' src='http://gaohr.win/site/special/2019/2019-09-27-idle-goods/images/avatar/a" + data_list[i].avatar + ".jpg' alt=''><a class='btn-mini' href='###' data-toggle='popover' title='" + data_list[i].owner + "' data-content='SOME INFO ABOUT OWNER' data-placement='top' data-trigger='focus' >点击此处，查看提供者信息</a></p></div></div></div>");
+	$("#data-list").append(showDataList(data_list, i));
 }
 	
-
+function showDataList(data_list, i) {
+	return "<div class='col-md-4' id='data-x-" + data_list[i].id + "' name='" + data_list[i].id + "'><div class='card'><p class='card_class'>" + data_list[i].type + "</p><a href='" + data_list[i].url + "'><img class='card_picture' src='../img/data/data-" + data_list[i].id + ".jpg' alt=''></a><p class='card_title'>" + data_list[i].name + "</p><div class='card-infos'><p class='card_text'>" + data_list[i].intro + "</p><p class='card_type'>数据格式：<span>" + data_list[i].format + "</span></p><p class='card_size'>数据大小：<span>" + data_list[i].size + "</span></p><p class='card_online'>获取方式：<span id='card_online_span_" + data_list[i].id + "'>" + data_list[i].online + "</span><hr><p class='card_owner'>数据提供者：<img class='seller_avatar' src='http://gaohr.win/site/special/2019/2019-09-27-idle-goods/images/avatar/a" + data_list[i].avatar + ".jpg' alt=''><a class='btn-mini' href='###' data-toggle='popover' title='" + data_list[i].owner + "' data-html='true' data-content='" + data_list[i].ownerinfo + "' data-placement='top' data-trigger='focus' >点击此处，查看提供者信息</a></p></div></div></div>"
+}
 	
 $("div[id^='data-x-']").hover(function() {
 	var xid = $(this).attr("name");
@@ -89,23 +130,75 @@ $("div[id^='data-x-']").hover(function() {
 });
 	
 	
-/*
-<div class="col-md-4" id='data-001'>
-	<div class='card'>
-		<p class='card_class'>遥感</p>
-		<img id='" + itemlist[i].id + "' class='card_picture' src='../img/data/data-001.jpg' alt=''>
-		<p class='card_title'>全球 1km 分辨率 DEM 数据<span class='card_online'>off</span></p>
-		<div class='card-infos'>
-			<p class='card_text'>SRTM数据主要是由美国航空航天局（NASA）和国防部国家测绘局（NIMA)联合测量的，SRTM的全称Shuttle Radar Topography Mission，即航天飞机雷达地形测绘使命。SRTM的数据是用16位的数值表示高程数值的，最大的正高程9000m，负高程海平面以下12000m。本数据格式为IMG格式。</p>
-			<p class='card_type'>数据类型：<span>xxx</span></p>
-			<p class='card_size'>数据大小：<span>xxx</span></p>
-			<p class='card_online'>获取方式：<span style='background:" + setColor_on_off(data_list[i].online) + "'>" + data_list[i].online + "</span>
-			<hr>
-			<p class='card_owner'>数据提供者：<img class='seller_avatar' src='http://gaohr.win/site/special/2019/2019-09-27-idle-goods/images/avatar/a029.jpg' alt=''><a class='btn-mini' href='###' data-toggle="popover" title="NAME" data-content="SOME INFO ABOUT OWNER" data-placement='top' data-trigger="focus" >点击此处，查看提供者信息</a></p>
-		</div>
-	</div>
-</div>
-*/
+	
+//Data tags
+var tagscon = "";
+var tagsarr = [];  //put tags into array
+var tagnum = [];  //same tags numeber and index
+for(i = 0; i < data_list.length; i++) {
+	for(t = 0; t < data_list[i].tags.length; t++) {
+		var has = tagsarr.indexOf(data_list[i].tags[t]);
+		if(has == "-1") {
+			tagsarr.push(data_list[i].tags[t]);
+		} else {
+			if(tagnum[has] == undefined) {
+					tagnum[has] = 2;
+				} else {
+					tagnum[has]++;
+				}
+		}
+	}
+}
+for(j = 0; j < tagsarr.length; j++) {
+	tagscon += "<a href='###' onclick=\"getData('" + tagsarr[j] + "')\" style='font-size:" + (14 + tagssize(tagnum[j])) + "px'><i class='icon ion-md-pricetag'></i> " + tagsarr[j] + "</a>";
+}
+$("#data-tags").append(tagscon);
+$("#data-tags").append("<br><a href='###' onclick=\"getAllData()\" style='font-size:16px;'>显示全部</a>");
+	
+//判断并确定标签大小
+function tagssize(n) {
+	if(n == undefined) {
+		return 0;
+	} else {
+		return n * 3;
+	}
+}
+	
+function getData(tag) {
+	alert("OK")
+}
+	
+function getAllData() {
+	// 先清空列表
+	$("#data-list").html("");
+	$('#data-list').append("<div class='col-md-4'></div><div class='col-md-4'><i id='data-loading' class='loading-3'></div><div class='col-md-4'></div>")
+	// 更新列表
+	setTimeout(function() {
+		$("#data-loading").remove();
+		for(var i = 0; i < data_list.length; i++) {
+			$("#data-list").append(showDataList(data_list, i));
+		}
+	}, 1500);
+	
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
