29d63f07ffb088330bc7f70ef5e3e28faf8f2f3a
gaohr <gaohrgao@163.com>
2019-11-02 16:52:40 +0800

    update

--- js/index.js
+++ js/index.js
@@ -12,6 +12,15 @@ $(function(){
 		$("#index-journal-list").append("<div class='index-journal-info'><a href='html/journal.html' target='_blank'><span class='rank-" + journal_rs[i].rank + "'>" + journal_rs[i].rank + "</span><span class='name'>" + journal_hydro[i].name + "</span><span class='type'>[水文]</span></a></div>");
 	}
 	
+	// conference
+	var conference_len = conference_list.length;
+	if(conference_len > 10){
+		conference_len = 10;
+	}
+	for(var i = 0; i < conference_len; i++) {
+		$("#index-conference-list").append("<div class='index-conference-info'><a href='" + conference_list[i].url + "' target='_blank'><img src='img/conference/c-" + conference_list[i].id + ".jpg' alt=''><span class='name'>" + conference_list[i].name + "</span><span class='type'>[" + conference_list[i].type + "会议 | " + conference_list[i].city + " | " + conference_list[i].country + "]</span></a></div>");
+	}
+	
 	// data
 	var data_len = data_list.length;
 	if(data_len > 10){
